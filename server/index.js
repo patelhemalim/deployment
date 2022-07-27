ef30e77e2cef7dfabbc98a24811b8451a166d4d6
Hemali Patel <patelhemalim@gmail.com>
2022-07-27 13:51:21 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -9,6 +9,15 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/css', (req, res) => {
+     res.sendFile(path.join(__dirname, '../public/styles.css'))
+ })
+ app.get('/js', (req, res) => {
+     res.sendFile(path.join(__dirname, '../public/main.js'))
+ })
+
+ app.use(express.static(path.join(__dirname, '../public')))
+
 const port = process.env.PORT || 4005
 
 app.listen(port,()=>{console.log(`Listening on port ${port}`)})
