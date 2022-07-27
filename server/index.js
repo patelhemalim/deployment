f3fb7384db4aeca34abf5324f4ff0d516121bde2
Hemali Patel <patelhemalim@gmail.com>
2022-07-27 15:43:27 -0500

    deploment trial

--- server/index.js
+++ server/index.js
@@ -6,15 +6,10 @@ const app = express()
 
 
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
-app.get('/css', (req, res) => {
-     res.sendFile(path.join(__dirname, '../public/styles.css'))
- })
- app.get('/js', (req, res) => {
-     res.sendFile(path.join(__dirname, '../public/main.js'))
- })
+
 
  app.use(express.static(path.join(__dirname, '../public')))
 
